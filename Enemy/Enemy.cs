1c7365518a38c6033923e9aa871eee7e9241df3e
ufukCardak <47851876+ufukCardak@users.noreply.github.com>
2023-10-04 05:58:27 +0300

    Update Enemy.cs

--- Enemy/Enemy.cs
+++ Enemy/Enemy.cs
@@ -1,1 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    protected Rigidbody2D rb;
+    protected Animator animator;
+    protected SpriteRenderer spriteRenderer;
+    protected Vector2 direction;
+
+    protected void FliptoPlayer(Transform rotateGameObject)
+    {
+        if (direction.x < 0)
+        {
+            rotateGameObject.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            rotateGameObject.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+    protected int PlayerDirection()
+    {
+        int movingRorL = 0;
+
+        if (direction.x < 0)
+        {
+            movingRorL = 1;
+        }
+        else
+        {
+            movingRorL = -1;
+        }
+
+        return movingRorL;
+    }
+}
 
